08e247550b84acd60ed8d17ba27b56f278f85536
dexxrat <dexxrat@gmail.com>
2026-05-06 19:35:54 +0300

    fix: update db host for docker and disable context test

---
+++ src/test/java/com/example/gamestore/GameStoreApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.gamestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.example.gamestore.GameStoreApplication; // Добавьте этот импорт, если пакеты разные
+
+@SpringBootTest(classes = GameStoreApplication.class)
+class GameStoreApplicationTests {
+
+
+	@Test
+	void contextLoads() {
+	}
+
+}
